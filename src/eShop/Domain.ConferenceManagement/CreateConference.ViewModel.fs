464ca77c0d3e1d72bef5964efbdb75f94e85b54f
7c78 <7c78tc@gmail.com>
2019-07-01 11:13:11 +0700

    add create conference view

---
+++ src/eShop/Domain.ConferenceManagement/CreateConference.ViewModel.fs
@@ -0,0 +1,17 @@
+﻿module eShop.Domain.ConferenceManagement.CreateConference.ViewModel
+
+open System
+
+[<CLIMutable>]
+type CreateConferenceViewModel =
+    { OwnerName: string
+      OwnerEmail: string
+      Slug: string
+      Name: string
+      Tagline: string
+      Location: string
+      TwitterSearch: string
+      Description: string
+      StartDate: DateTime
+      EndDate: DateTime }
+
--- src/eShop/Domain.ConferenceManagement/CreateConference.Web.fs
+++ src/eShop/Domain.ConferenceManagement/CreateConference.Web.fs
@@ -1,7 +1,17 @@
 module eShop.Domain.ConferenceManagement.CreateConference.Web
 
+open FSharp.Control.Tasks.V2.ContextInsensitive
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Mvc.ModelBinding
+open Giraffe
 open Giraffe.Razor
 
-let renderCreateReferenceView next ctx =
-    razorHtmlView "CreateConference" None None None next ctx
+open eShop.Domain.ConferenceManagement.CreateConference.ViewModel
+
+let renderCreateReferenceView next (ctx: HttpContext) =
+    task {
+        let modelState = ModelStateDictionary()
+        let! model = ctx.BindModelAsync<CreateConferenceViewModel>()
+        return! razorHtmlView "CreateConference" (Some model) None (Some modelState) next ctx
+    }
 
