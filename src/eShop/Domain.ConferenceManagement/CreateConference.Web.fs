445ce733213a5c0aaf8040ce167f58f482c76b86
7c78 <7c78tc@gmail.com>
2019-07-01 11:53:32 +0700

    correct

--- src/eShop/Domain.ConferenceManagement/CreateConference.Web.fs
+++ src/eShop/Domain.ConferenceManagement/CreateConference.Web.fs
@@ -1,17 +1,37 @@
 module eShop.Domain.ConferenceManagement.CreateConference.Web
 
+open System
 open FSharp.Control.Tasks.V2.ContextInsensitive
 open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.Mvc.ModelBinding
 open Giraffe
 open Giraffe.Razor
 
-open eShop.Domain.ConferenceManagement.CreateConference.ViewModel
+[<CLIMutable>]
+type CreateConferenceViewModel =
+    { OwnerName: string
+      OwnerEmail: string
+      Slug: string
+      Name: string
+      Tagline: string
+      Location: string
+      TwitterSearch: string
+      Description: string
+      StartDate: DateTime
+      EndDate: DateTime }
 
-let renderCreateReferenceView next (ctx: HttpContext) =
-    task {
-        let modelState = ModelStateDictionary()
-        let! model = ctx.BindModelAsync<CreateConferenceViewModel>()
-        return! razorHtmlView "CreateConference" (Some model) None (Some modelState) next ctx
-    }
+
+let renderCreateReferenceView next ctx =
+    let viewModel =
+        { OwnerName = ""
+          OwnerEmail = ""
+          Slug = ""
+          Name = ""
+          Tagline = ""
+          Location = ""
+          TwitterSearch = ""
+          Description = ""
+          StartDate = DateTime.UtcNow
+          EndDate = DateTime.UtcNow.AddDays(1.) }
+    razorHtmlView "CreateConference" (Some viewModel) None None next ctx
 
