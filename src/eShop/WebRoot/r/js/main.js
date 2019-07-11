dfb721299d46c5c03a71c3f18452d692f2f6e93c
Trung Do <trung.do@episerver.com>
2019-07-11 13:36:10 +0700

    remove unusable code

--- src/eShop/WebRoot/r/js/main.js
+++ src/eShop/WebRoot/r/js/main.js
@@ -31,53 +31,12 @@ $(document).ready(function () {
   function AnimateTile() {
     RSec = Math.floor(Math.random() * 5000) + 1000;
     RTile = Math.floor(Math.random() * 5);
-    setTimeout(function () {
-      $('.tile-slide').eq(RTile).cycle('next');
-      AnimateTile();
-    }, RSec);
+    setTimeout(
+      function () {
+        $('.tile-slide').eq(RTile).cycle('next');
+        AnimateTile();
+      },
+      RSec
+    );
   }
-});
-
-$(function () {
-  function getTweets() {
-    var $tweets = $("#tweets");
-    if ($tweets.length > 0) {
-      var search = $tweets.attr("data-search");
-      var url = 'http://search.twitter.com/search.json?callback=?&q=' + search;
-      $.getJSON(url, function (json) {
-        var output = [];
-        if (json.results) {
-          for (var i = 0, len = Math.min(json.results.length, 10); i < len; i++) {
-
-            var timeDifference = (new Date().getTime() - Date.parse(json.results[i].created_at)) / (60 * 1000);
-            var time;
-            if (timeDifference < 60) {
-              time = Math.round(timeDifference) + "m ago";
-            } else {
-              timeDifference = timeDifference / 60;
-              if (timeDifference < 24) {
-                time = Math.round(timeDifference) + "h ago";
-              } else {
-                time = Math.round(timeDifference / 24) + "d ago";
-              }
-            }
-            output.push('<span class="tile__tweet" style="left: 0px; top: 0px; display: inline; position: absolute;"><span class="tile__nick"><span class="tile__time">' + time + '</span>@' + json.results[i].from_user + '</span>' + json.results[i].text + '</span>');
-          }
-
-          //now select the #results element only once and append all the output at once, then slide it into view
-          $("#tweets").html(output.join(''));
-          $('.tile_twitter .tile-slide').cycle({
-            fx: 'scrollUp',
-            speed: 400,
-            timeout: 0
-          });
-        } else {
-          setTimeout(getTweets, 2000);
-        }
-      });
-    }
-  }
-
-  //run the getTweets function on document.ready
-  getTweets();
 });
